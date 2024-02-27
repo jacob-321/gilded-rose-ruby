896691a631964b0ef08992e8ed184ef3fb512914
Jacob Chaney <jacobchaneyschool@gmail.com>
2024-02-27 16:30:48 -0600

    Deleted the lengthy 'if' statement.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -11,58 +11,16 @@ class GildedRose
 
     case name
     when 'normal'
+      return normal_tick
     when 'Aged Brie'
+      return brie_tick
     when "Sulfuras, Hand of Ragnaros"
+      return sulfuras_tick
     when "Backstage passes to a TAFKAL80ETC concert"
       return backstage_tick
     end
-  end
 
-
-
-    if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
-      if @quality > 0
-        if @name != "Sulfuras, Hand of Ragnaros"
-          @quality = @quality - 1
-        end
-      end
-    else
-      if @quality < 50
-        @quality = @quality + 1
-        if @name == "Backstage passes to a TAFKAL80ETC concert"
-          if @days_remaining < 11
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-          if @days_remaining < 6
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-        end
-      end
-    end
-    if @name != "Sulfuras, Hand of Ragnaros"
-      @days_remaining = @days_remaining - 1
-    end
-    if @days_remaining < 0
-      if @name != "Aged Brie"
-        if @name != "Backstage passes to a TAFKAL80ETC concert"
-          if @quality > 0
-            if @name != "Sulfuras, Hand of Ragnaros"
-              @quality = @quality - 1
-            end
-          end
-        else
-          @quality = @quality - @quality
-        end
-      else
-        if @quality < 50
-          @quality = @quality + 1
-        end
-      end
-    end
+    if @name != 'Aged Brie' && @name != "Backstage passes to a TAFKAL80ETC concert"
   end
 
   def normal_tick
