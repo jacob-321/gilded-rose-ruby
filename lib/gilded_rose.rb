ecd093ff4a1d68149e780b562e8e8b081a1cb9af
Jacob Chaney <jacobchaneyschool@gmail.com>
2024-02-27 16:01:34 -0600

    Completed the normal_tick function so that refactoring reaches green.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -57,7 +57,14 @@ class GildedRose
     end
   end
   def normal_tick
-    @quality -= 1
+    if @quality != 0
+      if @days_remaining > 0
+        @quality -= 1
+      end
+      if @days_remaining <= 0
+        @quality -= 2
+      end
+    end
     @days_remaining -= 1
   end
 end
