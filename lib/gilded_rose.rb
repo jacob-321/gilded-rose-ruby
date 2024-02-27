3faca0e7bf822f72eeece87ee5266b18f11d0d2d
Jacob Chaney <jacobchaneyschool@gmail.com>
2024-02-27 16:17:28 -0600

    Completed brie_tick

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,9 +8,24 @@ class GildedRose
   end
 
   def tick
-    if name == "normal"
-      return
+
+    case name
+    when 'normal'
+      return normal_tick
+    when 'Aged Brie'
+      return brie_tick
     end
+  end
+
+  def brie_tick
+    @days_remaining -= 1
+    return if @quality >= 50
+
+    @quality += 1
+    @quality += 1 if @days_remaining <= 0
+  end
+
+
 
     if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
       if @quality > 0
@@ -56,6 +71,7 @@ class GildedRose
       end
     end
   end
+
   def normal_tick
     @days_remaining -= 1
     return if @quality == 0
