4f2eb12a6ef076182a1ac6c1855546b16c406d77
Jacob Chaney <jacobchaneyschool@gmail.com>
2024-02-27 22:36:08 -0600

    Added Conjured class

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,15 +1,21 @@
 class GildedRose
 
+  class Conjured < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+
+      @quality -= 2
+      @quality -= 2 if @days_remaining <= 0
+    end
+  end
+
   DEFAULT_CLASS = Item
   SPECIALIZED_CLASSES = {
     'normal'                                    => Normal,
     'Aged Brie'                                 => Brie,
-    'Backstage passes to a TAFKAL80ETC concert' => Backstage }
-
-  class Item
-    def tick
-    end
-  end
+    'Backstage passes to a TAFKAL80ETC concert' => Backstage,
+    'Conjured Mana Cake'                        => Conjured }
 
   def self.for(name, quality, days_remaining)
     (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS).
