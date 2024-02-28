bec32cb77f4ae61d35b9e6d7ef5b4e6acd15a91b
Jacob Chaney <jacobchaneyschool@gmail.com>
2024-02-27 22:48:45 -0600

    Finalized code for submission

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,4 +1,47 @@
-class GildedRose
+module GildedRose
+  class Item
+    attr_reader :quality, :days_remaining
+
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+
+    def tick
+    end
+  end
+
+  class Normal < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+
+      @quality -= 1
+      @quality -= 1 if @days_remaining <= 0
+    end
+  end
+
+  class Brie < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50
+
+      @quality += 1
+      @quality += 1 if @days_remaining <= 0
+    end
+  end
+
+  class Backstage < Item
+    def tick
+      @days_remaining -= 1
+      return                    if quality >= 50
+      return @quality = 0       if quality < 0
+
+      @quality += 1
+      @quality += 1 if @days_remaining < 10
+      @quality += 1 if @days_remaining < 15
+    end
+  end
+
 
   class Conjured < Item
     def tick
