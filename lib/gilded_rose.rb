6315c70115140a48de29f0206ea80f28ab7d9c70
Jacob Chaney <jacobchaneyschool@gmail.com>
2024-02-27 22:22:41 -0600

    Added DEFAULT_CLASS and SPECIALIZED_CLASSES

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,8 +1,19 @@
 class GildedRose
 
+  DEFAULT_CLASS = Item
+  SPECIALIZED_CLASSES = {
+    'normal'                                    => Normal,
+    'Aged Brie'                                 => Brie,
+    'Backstage passes to a TAFKAL80ETC concert' => Backstage }
+
   class Item
     def tick
     end
+  end
+
+  def self.for(name, quality, days_remaining)
+    klass_for(name).
+      new(quality, days_remaining)
   end
 
   def self.klass_for(name)
