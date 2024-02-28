99ae7839f789fdef8b4b90966dbcfd08b62f3855
Jacob Chaney <jacobchaneyschool@gmail.com>
2024-02-27 22:18:35 -0600

    Removed Sulfuras class

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -5,7 +5,17 @@ class GildedRose
     end
   end
 
-  class Sulfuras < Item
-    def tick
+  def self.klass_for(name)
+    case name
+    when 'normal'
+      Normal
+    when 'Aged Brie'
+      Brie
+    when 'Sulfuras, Hand of Ragnaros'
+      Item
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      Backstage
     end
   end
+
+end
