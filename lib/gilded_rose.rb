aec2c78b67be9a12e4c3498bac0fb435390667a5
Jacob Chaney <jacobchaneyschool@gmail.com>
2024-02-27 21:02:42 -0600

    Changed initialize to self.for and changed classes

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,11 +1,11 @@
 class GildedRose
-  attr_reader :name, :item
+  attr_reader :item
 
-  def initialize(name, quality, days_remaining)
-    @item = klass_for(name).new(quality, days_remaining)
+  def self.for(name, quality, days_remaining)
+    klass_for(name).new(quality, days_remaining)
   end
 
-  def klass_for(name)
+  def self.klass_for(name)
     case name
     when 'normal'
       Normal
@@ -18,78 +18,20 @@ class GildedRose
     end
   end
 
-
-  class Brie
-    attr_reader :quality, :days_remaining
-
-    def initialize(quality, days_remaining)
-      @quality, @days_remaining = quality, days_remaining
-    end
-
-    def tick
-      @days_remaining -= 1
-      return if @quality >= 50
-
-      @quality += 1
-      @quality += 1 if @days_remaining <= 0
-    end
+  def tick
+    item.tick
   end
 
   def quality
-    return item.quality if item
-    @quality
+    item.quality
   end
 
   def days_remaining
-    return item.days_remaining if item
-    @days_remaining
+    item.days_remaining
   end
 
   class Normal
-    attr_reader :quality, :days_remaining
-
-    def initialize(quality, days_remaining)
-      @quality, @days_remaining = quality, days_remaining
-    end
-
-
-  def sulfuras_tick
-    @item = Sulfuras.new(quality, days_remaining)
-    item.tick
-  end
-
+  class Brie
   class Sulfuras
-    attr_reader :quality, :days_remaining
-
-    def initialize(quality, days_remaining)
-      @quality, @days_remaining = quality, days_remaining
-    end
-
-    def tick
-    end
-  end
-
-
-  def backstage_tick
-    @item = Sulfuras.new(quality, days_remaining)
-    item.tick
-  end
-
   class Backstage
-    attr_reader :quality, :days_remaining
-
-    def initialize(quality, days_remaining)
-      @quality, @days_remaining = quality, days_remaining
-    end
-
-    def tick
-      @days_remaining -= 1
-      return                if @quality >= 50
-      return @quality = 0   if @days_remaining < 0
-
-      @quality += 1
-      @quality += 1 if @days_remaining < 10
-      @quality += 1 if @days_remaining < 5
-    end
-
 end
