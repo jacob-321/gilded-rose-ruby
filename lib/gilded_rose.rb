a3985dd327d9b5d67212d968f34d27b2c79589e5
Jacob Chaney <jacobchaneyschool@gmail.com>
2024-02-27 20:22:43 -0600

    Defined quality and days_remaining.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,10 +1,27 @@
 class GildedRose
   attr_reader :name, :days_remaining, :quality
 
-  def initialize(name:, days_remaining:, quality:)
-    @name = name
-    @days_remaining = days_remaining
-    @quality = quality
+  def normal_tick
+    @item = Normal.new(quality, days_remaining)
+    item.tick
+  end
+
+  def brie_tick
+    @days_remaining -= 1
+    return if @quality >= 50
+
+    @quality += 1
+    @quality += 1 if @days_remaining <= 0
+  end
+
+  def quality
+    return item.quality if item
+    @quality
+  end
+
+  def days_remaining
+    return item.days_remaining if item
+    @days_remaining
   end
 
   class Normal
@@ -15,35 +32,11 @@ class GildedRose
     end
 
   def tick
-
-    case name
-    when 'normal'
-      return normal_tick
-    when 'Aged Brie'
-      return brie_tick
-    when "Sulfuras, Hand of Ragnaros"
-      return sulfuras_tick
-    when "Backstage passes to a TAFKAL80ETC concert"
-      return backstage_tick
-    end
-
-    if @name != 'Aged Brie' && @name != "Backstage passes to a TAFKAL80ETC concert"
-  end
-
-  def tick
     @days_remaining -= 1
     return if @quality == 0
 
     @quality -= 1
     @quality -= 1 if @days_remaining <= 0
-  end
-
-  def brie_tick
-    @days_remaining -= 1
-    return if @quality >= 50
-
-    @quality += 1
-    @quality += 1 if @days_remaining <= 0
   end
 
   def sulfuras_tick
