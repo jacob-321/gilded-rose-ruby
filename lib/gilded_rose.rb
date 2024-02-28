1f72330b8c5da99162c25eb12fd4e489bdda280a
Jacob Chaney <jacobchaneyschool@gmail.com>
2024-02-27 22:15:11 -0600

    Removed everything except Item and Sulfuras class.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,56 +1,11 @@
 class GildedRose
 
-  def self.for(name, quality, days_remaining)
-    klass_for(name).new(quality, days_remaining)
-  end
-
-  def self.klass_for(name)
-    case name
-    when 'normal'
-      Normal
-    when 'Aged Brie'
-      Brie
-    when 'Sulfuras, Hand of Ragnaros'
-      Sulfuras
-    when 'Backstage passes to a TAFKAL80ETC concert'
-      Backstage
-    end
-  end
-
   class Item
-    attr_reader :quality, :days_remaining
-
-    def initializes(quality, days_remaining)
-      @quality, @days_remaining = quality, days_remaining
-    end
-
-  class Normal < Item
-    def tick
-      @days_remaining -= 1
-      return if @quality == 0
-
-      @quality -= 1
-      @quality -= 1 if @days_remaining <= 0
-    end
-  end
-
-    class Brie < Item
-    def tick
-      @days_remaining -= 1
-      return if @quality >= 50
-
-      @quality += 1
-      @quality += 1 if @days_remaining <= 0
-    end
-  end
-
-    class Sulfuras < Item
     def tick
     end
   end
 
-  class Backstage < Item
+  class Sulfuras < Item
     def tick
     end
   end
-end
