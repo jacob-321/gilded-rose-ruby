b4dce548db41f2dc89ffa95ed65ed07b932c767b
Jacob Chaney <jacobchaneyschool@gmail.com>
2024-02-27 20:50:34 -0600

    Added klass_for(name)

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,20 +1,20 @@
 class GildedRose
-  attr_reader :name, :days_remaining, :quality
+  attr_reader :name, :item
 
-  def tick
+  def initialize(name, quality, days_remaining)
+    @item = klass_for(name).new(quality, days_remaining)
+  end
+
+  def klass_for(name)
     case name
     when 'normal'
-      @item = Normal.new(quality, days_remaining)
-      item.tick
+      Normal
     when 'Aged Brie'
-      @item = Brie.new(quality, days_remaining)
-      item.tick
+      Brie
     when 'Sulfuras, Hand of Ragnaros'
-      @item = Sulfuras.new(quality, days_remaining)
-      item.tick
+      Sulfuras
     when 'Backstage passes to a TAFKAL80ETC concert'
-      @item = Backstage.new(quality, days_remaining)
-      item.tick
+      Backstage
     end
   end
 
