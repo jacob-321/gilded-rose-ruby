29af0619a90a6e0f4f7215f5547cc73c85e23dde
Jacob Chaney <jacobchaneyschool@gmail.com>
2024-02-27 20:29:45 -0600

    Redid brie_tick and added Brie class

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -7,11 +7,24 @@ class GildedRose
   end
 
   def brie_tick
-    @days_remaining -= 1
-    return if @quality >= 50
+    @item = Brie.new(quality, days_remaining)
+    item.tick
+  end
 
-    @quality += 1
-    @quality += 1 if @days_remaining <= 0
+  class Brie
+    attr_reader :quality, :days_remaining
+
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50
+
+      @quality += 1
+      @quality += 1 if @days_remaining <= 0
+    end
   end
 
   def quality
@@ -31,13 +44,6 @@ class GildedRose
       @quality, @days_remaining = quality, days_remaining
     end
 
-  def tick
-    @days_remaining -= 1
-    return if @quality == 0
-
-    @quality -= 1
-    @quality -= 1 if @days_remaining <= 0
-  end
 
   def sulfuras_tick
   end
