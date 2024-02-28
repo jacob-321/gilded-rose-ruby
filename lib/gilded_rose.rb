f4aff5ddb53348158de1897ab997fff49b27ae13
Jacob Chaney <jacobchaneyschool@gmail.com>
2024-02-27 22:25:55 -0600

    Changed the klass function

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -12,21 +12,8 @@ class GildedRose
   end
 
   def self.for(name, quality, days_remaining)
-    klass_for(name).
+    (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS).
       new(quality, days_remaining)
-  end
-
-  def self.klass_for(name)
-    case name
-    when 'normal'
-      Normal
-    when 'Aged Brie'
-      Brie
-    when 'Sulfuras, Hand of Ragnaros'
-      Item
-    when 'Backstage passes to a TAFKAL80ETC concert'
-      Backstage
-    end
   end
 
 end
